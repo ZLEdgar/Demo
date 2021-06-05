2fb7ea1540e11df7cf0821ff69f5559e39e74190
luojing58 <cdluojing5@jd.com>
2021-06-05 09:01:13 +0800

    初始化

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 罗静 on 2021/6/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Demo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by 罗静 on 2021/6/5.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
