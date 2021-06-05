843c9ae6f74c3dde7a9f3e2752c8e4a70881a0b8
luojing58 <cdluojing5@jd.com>
2021-06-05 09:08:16 +0800

    spec

---
+++ Demo/Classes/TextDemo.h
@@ -0,0 +1,18 @@
+//
+//  TextDemo.h
+//  Demo
+//
+//  Created by 罗静 on 2021/6/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextDemo : NSObject
+
+- (void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo/Classes/TextDemo.m
@@ -0,0 +1,16 @@
+//
+//  TextDemo.m
+//  Demo
+//
+//  Created by 罗静 on 2021/6/5.
+//
+
+#import "TextDemo.h"
+
+@implementation TextDemo
+
+- (void)log {
+    NSLog(@"----------");
+}
+
+@end
